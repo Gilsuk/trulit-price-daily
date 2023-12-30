4732795d1ccd816ae9489e2cf05c5c3af8849425
gilsuk <devgilsuk@gmail.com>
2023-12-30 21:18:38 +0900

    re-describe error msg

--- collector/presenter/handler/error.go
+++ collector/presenter/handler/error.go
@@ -2,5 +2,5 @@ package handler
 
 import "errors"
 
-var ErrInvalidDateFormat error = errors.New("invalid date format")
+var ErrInvalidDateFormat error = errors.New("invalid date format. the format should be yyyy-MM-dd")
 var ErrInvalidJsonSyntax error = errors.New("invalid json syntax")
